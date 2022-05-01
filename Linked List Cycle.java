14e3b5473235cd23bb59f118d9f7e4a30928807d
Payal kataruka <33001727+payal1994@users.noreply.github.com>
2022-05-01 13:13:40 +0530

    Create Linked List Cycle.java

---
+++ Linked List Cycle.java
@@ -0,0 +1,122 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+Solution:
+Approach 1: Hash Table
+Intuition
+
+To detect if a list is cyclic, we can check whether a node had been visited before. A natural way is to use a hash table.
+
+Algorithm
+
+We go through each node one by one and record each node's reference (or memory address) in a hash table. If the current node is null, we have reached the end of the list and it must not be cyclic. If current node’s reference is in the hash table, then return true.
+
+
+Complexity analysis
+
+Let nn be the total number of nodes in the linked list.
+
+Time complexity : O(n)O(n). We visit each of the nn elements in the list at most once. Adding a node to the hash table costs only O(1)O(1) time.
+
+Space complexity: O(n)O(n). The space depends on the number of elements added to the hash table, which contains at most nn elements.
+
+
+Approach 2: Floyd's Cycle Finding Algorithm
+Intuition
+
+Imagine two runners running on a track at different speed. What happens when the track is actually a circle?
+
+Algorithm
+
+The space complexity can be reduced to O(1)O(1) by considering two pointers at different speed - a slow pointer and a fast pointer. The slow pointer moves one step at a time while the fast pointer moves two steps at a time.
+
+If there is no cycle in the list, the fast pointer will eventually reach the end and we can return false in this case.
+
+Now consider a cyclic list and imagine the slow and fast pointers are two runners racing around a circle track. The fast runner will eventually meet the slow runner. Why? Consider this case (we name it case A) - The fast runner is just one step behind the slow runner. In the next iteration, they both increment one and two steps respectively and meet each other.
+
+How about other cases? For example, we have not considered cases where the fast runner is two or three steps behind the slow runner yet. This is simple, because in the next or next's next iteration, this case will be reduced to case A mentioned above.
+
+
+Complexity analysis
+
+Time complexity : O(n)O(n). Let us denote nn as the total number of nodes in the linked list. To analyze its time complexity, we consider the following two cases separately.
+
+List has no cycle:
+The fast pointer reaches the end first and the run time depends on the list's length, which is O(n)O(n).
+
+List has a cycle:
+We break down the movement of the slow pointer into two steps, the non-cyclic part and the cyclic part:
+
+The slow pointer takes "non-cyclic length" steps to enter the cycle. At this point, the fast pointer has already reached the cycle. \text{Number of iterations} = \text{non-cyclic length} = NNumber of iterations=non-cyclic length=N
+
+Both pointers are now in the cycle. Consider two runners running in a cycle - the fast runner moves 2 steps while the slow runner moves 1 steps at a time. Since the speed difference is 1, it takes \dfrac{\text{distance between the 2 runners}}{\text{difference of speed}} 
+difference of speed
+distance between the 2 runners
+​
+  loops for the fast runner to catch up with the slow runner. As the distance is at most "\text{cyclic length K}cyclic length K" and the speed difference is 1, we conclude that
+\text{Number of iterations} = \text{almost}Number of iterations=almost "\text{cyclic length K}cyclic length K".
+
+Therefore, the worst case time complexity is O(N+K)O(N+K), which is O(n)O(n).
+
+Space complexity : O(1)O(1). We only use two nodes (slow and fast) so the space complexity is O(1)O(1).
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set =new HashSet<ListNode>();
+            while(head!=null){
+        if(set.contains(head))
+            return true;
+        else{
+            set.add(head);
+            head=head.next;
+        }
+            
+       }
+        return false;
+    }
+}
